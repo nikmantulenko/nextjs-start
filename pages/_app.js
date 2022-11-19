b8e4a05ee59d08ffc311a82160db1030b708e259
nikmant <nikmantulenko@gmail.com>
2022-11-19 22:07:20 +0200

    learned how to deal with fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import Script from 'next/script'
+import { Roboto } from '@next/font/google'
 import '../styles/globals.css'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+})
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script src="https://connect.facebook.net/en_US/sdk.js" strategy="lazyOnload" />
+      <style jsx global>{`
+        html {
+          font-family: ${roboto.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
     </>
   )
